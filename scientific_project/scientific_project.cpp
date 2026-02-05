3f0818e84d281736bd428f0b03d102494a1d8cf8
NicaZadirieva <no.demchenko@yandex.ru>
2026-02-05 10:08:06 +0300

    chore: init

---
+++ scientific_project/scientific_project.cpp
@@ -0,0 +1,12 @@
+﻿// scientific_project.cpp: определяет точку входа для приложения.
+//
+
+#include "scientific_project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
