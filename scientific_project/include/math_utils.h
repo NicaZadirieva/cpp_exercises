96692809b9bdd86f0598de7bbf4daaea403c69fc
NicaZadirieva <no.demchenko@yandex.ru>
2026-02-05 10:44:17 +0300

    chore: init new task

--- scientific_project/include/math_utils.h
+++ scientific_project/include/math_utils.h
@@ -3,14 +3,5 @@
 #include <iostream>
 #include <cmath>
 
-// Константа для размера массива
-const int ARRAY_LENGTH = 10;
-
-// Функция для создания и заполнения массива
-double* create_array();
-
-// Функция для вычисления среднего значения массива
-double calc_average(double* arr);
-
 // Главная функция - точка входа
 void entrypoint();
--- scientific_project/src/math_utils.cpp
+++ scientific_project/src/math_utils.cpp
@@ -1,29 +1,11 @@
-﻿// 1. Создайте массив из 10 double в куче (new)
-// 2. Заполните его значениями синуса: arr[i] = sin(i * 0.1)
-// 3. Вычислите среднее значение
-// 4. Не забудьте delete[]
+﻿// Дана строка: "3.14,2.71,1.41,0.577"
+// 1. Разделите её на числа с помощью std::getline и std::stringstream
+// 2. Сохраните в vector<double>
+// 3. Найдите максимальное и минимальное значение
 #include "../include/math_utils.h"
 using namespace std;
 
-double* create_array() {
-	double* arr = new double[ARRAY_LENGTH];
-	for (int i = 0; i < ARRAY_LENGTH; i++) {
-		arr[i] = sin(i * 0.1);
-	}
-	return arr;
-}
-
-double calc_average(double* arr) {
-	double sum = 0;
-	for (int i = 0; i < 10; i++) {
-		sum += arr[i];
-	}
-	return sum / ARRAY_LENGTH;
-}
 
 void entrypoint() {
-	double* arr = create_array();
-	cout << "average = " << calc_average(arr);
 
-	delete[] arr;
 }
