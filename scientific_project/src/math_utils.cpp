088aba1c4b108d3b3f5efe1422eaa379be675417
NicaZadirieva <no.demchenko@yandex.ru>
2026-02-05 11:18:03 +0300

    chore: init task

--- scientific_project/src/math_utils.cpp
+++ scientific_project/src/math_utils.cpp
@@ -1,65 +1,12 @@
-﻿// 1. Создайте vector<double> measurements = {12.5, 13.2, 11.9, 12.8, 13.1, 100.0}
-// 2. Удалите "выбросы" - значения, отличающиеся от медианы более чем на 3 сигмы
-// 3. Отсортируйте оставшиеся значения
-// 4. Вычислите среднее и стандартное отклонение
+﻿// 1. Создайте map<string, double> experiment_params
+// 2. Добавьте: {"temperature", 298.15}, {"pressure", 1.013}, {"time", 3600.0}
+// 3. Напишите функцию, которая выводит все параметры в формате "key = value units"
+// 4. Проверьте наличие ключа "pH", если нет - добавьте со значением 7.0
 #include "../include/math_utils.h"
 
 using namespace std;
 
-void print_vector(vector<double> numbers) {
-    for (double number : numbers) {
-        cout << number << endl;
-    }
-}
-
-double calc_mediana(vector<double> numbers) {
-    vector<double> sorted_numbers = numbers;
-    sort(sorted_numbers.begin(), sorted_numbers.end());
-    const int vector_size = sorted_numbers.size();
-    if (vector_size % 2 == 0) {
-        return (double)(sorted_numbers[(vector_size - 1) / 2] + sorted_numbers[vector_size / 2]) / 2.0;
-    }
-    else {
-        return (double)sorted_numbers[vector_size / 2];
-    }
-}
- 
-vector<double> remove_outliers(vector<double> numbers, double sigma) {
-    double mediana = calc_mediana(numbers);
-    vector<double> res = {};
-    for (double number : numbers) {
-        if (abs(number - mediana) <= 3 * sigma) {
-            res.push_back(number);
-        }
-    }
-    sort(res.begin(), res.end());
-    return res;
-}
-
-double calc_standard_deviation(const vector<double>& numbers) {
-    double sum = 0.0, mean, standardDeviation = 0.0;
-    int size = numbers.size();
-    // Вычислить сумму элементов в векторе
-    for (int i = 0; i < size; ++i) {
-        sum += numbers[i];
-    }
-    // Вычислить среднее значение
-    mean = sum / size;
-    // Вычислить сумму квадратов отклонений от среднего значения
-    for (int i = 0; i < size; ++i) {
-        standardDeviation += pow(numbers[i] - mean, 2);
-    }
-    // Вычислить квадратный корень из дисперсии, чтобы получить стандартное отклонение
-    return sqrt(standardDeviation / size);
-}
-
 
 void entrypoint() {
-    vector<double> measurements = { 12.5, 13.2, 11.9, 12.8, 13.1, 100.0 };
-    vector<double> processed_measurements = remove_outliers(measurements, 0.1);
-    cout << "Measurements: " << endl;
-    print_vector(measurements);
-    cout << "After removing: " << endl;
-    print_vector(processed_measurements);
-    cout << "StandardDeviation: " << calc_standard_deviation(measurements);
+
 }
