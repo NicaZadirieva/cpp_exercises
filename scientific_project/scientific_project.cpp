cafe57f705c8dc40743eb5d2d96270f2194469b1
NicaZadirieva <no.demchenko@yandex.ru>
2026-02-07 23:16:50 +0300

    feat: реализация производной функции (с ошибками)

--- scientific_project/scientific_project.cpp
+++ scientific_project/scientific_project.cpp
@@ -6,6 +6,25 @@
 
 using namespace std;
 
+double calculate_delta_x(vector<double> &xs,
+    vector<double> &ys) {
+    double delta_x = xs[1] - xs[0];
+    double delta_y = ys[1] - ys[0];
+    double epsilon = 0.1;
+    bool found = false;
+    while (!found) {
+        for (int i = 1; i < xs.size() - 1; i++) {
+            if (abs((ys[i + 1] - ys[i]) / delta_x - delta_y) > epsilon) {
+                delta_x = xs[i + 1] - xs[i];
+                break;
+            }
+        }
+        found = true;
+    }
+    return delta_x;
+}
+
+
 
 int main() {
     ScientificFile file("data/function_points.csv", ios::in);
@@ -34,6 +53,17 @@ int main() {
 
     SimpleGnuplot::plot_data(xs, ys, "fx");
 
+    auto delta_x = calculate_delta_x(xs, ys);
+    vector<double> dxs = vector<double>(xs.size());
+    vector<double> dys = vector<double>(ys.size());
+    for (int i = 0; i < xs.size() - 1; i++) {
+        dxs.push_back(xs[i + 1]);
+        dys.push_back((ys[i + 1] - ys[i]) / delta_x);
+    }
+    cout << endl << "dys: " << endl;
+    copy(dys.begin(), dys.end(), ostream_iterator<double>(cout, " "));
 
+
+    SimpleGnuplot::plot_data(dxs, dys, "dfx");
     return 0;
 }
